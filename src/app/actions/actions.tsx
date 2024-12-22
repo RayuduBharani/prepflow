44680af89c0d2919bda64a96117ae3645694564f
Rayudu Bharani <rayudubharani7288@gmail.com>
2024-12-22 13:51:44 +0530

    Added user role change functionality

---
+++ src/app/actions/actions.tsx
@@ -0,0 +1,26 @@
+"use server"
+import { PrismaClient } from '@prisma/client';
+import { redirect } from 'next/navigation';
+const prisma = new PrismaClient()
+
+export async function changeToAdmin(formData: FormData) {
+    const user = await prisma.user.findUnique({
+        where: { email: formData.get("email") as string }
+    })
+    if (!user) {
+        return console.log("user not found")
+    }
+    if (user.role == "USER") {
+        await prisma.user.update({
+            where: { email: formData.get("email") as string },
+            data: { role: "ADMIN" }
+        })
+    }
+    else {
+        await prisma.user.update({
+            where: { email: formData.get("email") as string },
+            data: { role: "USER" }
+        })
+    }
+    redirect("/")
+}
