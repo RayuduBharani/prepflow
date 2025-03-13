f0d9e3dacb844cf590f60ca1608f9cf523a1975c
Ashok <56536970+cygnuxxs@users.noreply.github.com>
2025-03-13 11:48:58 +0530

    Changes x3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,7 @@ import ReactQuery from "./ReactQuery";
 const Footer = dynamic(() => import('@/components/Footer'))
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets : ['latin'],
 });
 
 export const metadata: Metadata = {
