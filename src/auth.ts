aa7e8e89799b8c099ea3b077fb992fc3abfb75d2
Ashok <56536970+cygnuxxs@users.noreply.github.com>
2025-03-14 09:21:13 +0530

    Changed in auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -65,23 +65,24 @@ export const { signIn, signOut, auth, handlers } = NextAuth({
     },
   },
   session: {
-    strategy: "jwt",
+    strategy: "database",
   },
-  cookies: {
-    sessionToken: {
-      name: "__Secure-authjs.session-token",
-      options: {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "lax",
-        path: "/",
-        // Only set domain if you're sure it matches your production domain
-        domain: process.env.NODE_ENV === "production" ? "prepflow.vercel.app" : undefined,
-      },
-    },
-  },
+  // cookies: {
+  //   sessionToken: {
+  //     name: "__Secure-authjs.session-token",
+  //     options: {
+  //       httpOnly: true,
+  //       secure: process.env.NODE_ENV === "production",
+  //       sameSite: "lax",
+  //       path: "/",
+  //       // Only set domain if you're sure it matches your production domain
+  //       domain: process.env.NODE_ENV === "production" ? "vercel.app" : undefined,
+  //     },
+  //   },
+  // },
   secret: process.env.AUTH_SECRET,
   pages: {
     signIn: "/signin",
   },
+  trustHost : true,
 });
