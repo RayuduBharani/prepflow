aa5c203ec3bb1430714774f5be7ec6966dfdf21d
Rayudu Bharani <rayudubharani7288@gmail.com>
2025-08-11 14:13:53 +0530

    again added render backend link

--- src/app/(user)/compiler/compiler.tsx
+++ src/app/(user)/compiler/compiler.tsx
@@ -85,7 +85,7 @@ const PythonCompiler: React.FC = () => {
     
     console.log(code, inputs);
     try {
-      const response = await fetch('http://65.0.76.164:8000/run-python', {
+      const response = await fetch('https://aws-compiler.onrender.com/run-python', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -119,7 +119,7 @@ const PythonCompiler: React.FC = () => {
 
   const testBackendConnection = async () => {
     try {
-      const response = await fetch('http://65.0.76.164:8000/');
+      const response = await fetch('https://aws-compiler.onrender.com/');
       if (response.ok) {
         setBackendStatus('connected');
         setError('');
