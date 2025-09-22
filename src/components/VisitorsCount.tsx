b89d470d9f499548c194b4870550694793097d85
Ashok <56536970+cygnuxxs@users.noreply.github.com>
2025-09-22 23:51:23 +0530

    Changed VisitorsCount typo

--- src/components/VisitorsCount.tsx
+++ src/components/VisitorsCount.tsx
@@ -1,20 +1,20 @@
-// import { getVisitorsCount } from "@/actions/visitorsActions";
+import { getVisitorsCount } from "@/actions/visitorsActions";
 import { formatIndianCount, getLabel } from "@/lib/utils";
 import React from "react";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./ui/tooltip";
 
 const VisitorsCount = async () => {
-  // const count = await getVisitorsCount();
+  const count = await getVisitorsCount();
   return (
     <span className="text-xs px-2 py-1 w-full text-start font-medium text-muted-foreground flex items-center gap-4">
       Visitors
       <TooltipProvider>
         <Tooltip delayDuration={100}>
           <TooltipTrigger asChild>
-<span className="text-primary font-bold text-xl">{formatIndianCount(1)}</span>
+<span className="text-primary font-bold text-xl">{formatIndianCount(count)}</span>
           </TooltipTrigger>
           <TooltipContent side="top">
-            <p>{1} {getLabel(1)}</p>
+            <p>{count} {getLabel(count)}</p>
           </TooltipContent>
         </Tooltip>
         </TooltipProvider>
