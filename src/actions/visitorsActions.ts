8e184e51494b302051a87ae6d8f37c5566fb8dd1
Ashok <56536970+cygnuxxs@users.noreply.github.com>
2025-09-23 00:38:46 +0530

    Fix in visitors;

--- src/actions/visitorsActions.ts
+++ src/actions/visitorsActions.ts
@@ -6,9 +6,7 @@ export async function getVisitorsCount() {
     const baseUrl = process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000";
     const apiUrl = `${baseUrl}/api/visitors`;
 
-    const res = await fetch(apiUrl, {
-      next: { revalidate: 10 }, // Revalidate every hour
-    });
+    const res = await fetch(apiUrl, {cache : 'no-store'});
 
     if (!res.ok) {
       console.error(`Failed to fetch visitors count: ${res.status} ${res.statusText}`);
@@ -16,13 +14,14 @@ export async function getVisitorsCount() {
     }
 
     const data = await res.json();
-        if (typeof data?.count !== 'number') {
+    console.log(data)
+        if (typeof data?.activeCount !== 'number') {
       throw new Error('Invalid response format: count is not a number');
     }
 
-    const { count }: { count: number } = data;
+    const { activeCount }: { activeCount : number } = data;
 
-    return count;
+    return activeCount;
   } catch (error) {
     console.error('Error in getVisitorsCount:', error);
     return 0;
