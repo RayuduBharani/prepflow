8f4c5c58e9377919d6eafd4a403d49e0dee529be
Ashok <56536970+cygnuxxs@users.noreply.github.com>
2025-09-23 20:18:11 +0530

    Fixed typo in VisitorsCount

--- src/components/VisitorsCount.tsx
+++ src/components/VisitorsCount.tsx
@@ -4,8 +4,7 @@ import React from "react";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./ui/tooltip";
 
 const VisitorsCount = async () => {
-  // const count = await getVisitorsCount();
-  const count=1
+  const count = await getVisitorsCount();
   return (
     <span className="text-xs px-2 py-1 w-full text-start font-medium text-muted-foreground flex items-center gap-4">
       Visitors
